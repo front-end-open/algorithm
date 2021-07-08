e892a038fb7b991dbb599d07a1ac5f18141bf942
wangshan <22212@qq.com>
2021-07-09 00:09:04 +0800

    refactor: 重构代码，理论上不影响现有功能（即不是新增功能，也不是修改bug的代码变动）
    - 比较函数添加泛型函数类型
    - 泛型参数约束

--- src/sort/day_6.ts
+++ src/sort/day_6.ts
@@ -2,7 +2,7 @@
  * @Author: wangshan
  * @Date: 2021-07-07 23:07:17
  * @LastEditors: wangshan
- * @LastEditTime: 2021-07-08 22:31:15
+ * @LastEditTime: 2021-07-08 22:53:56
  * @Description: 分布式排序-计数排序
  * @go: 主要用来排序整数序列
  */
@@ -11,13 +11,15 @@
  创建临时数据，根据数组最大值。
  数组索引顺序排列，经过计数后的数组，已经就排好序了。然后循环递减拿出每个索引的值，放回原数组即可。
 */
+import { findMaxValue } from "../utils/index";
+
 function countingSort(array: number[]) {
   if (array.length < 2) {
     return array;
   }
   const maxValue = findMaxValue(array);
 
-  const counts = new Array(maxValue + 1);
+  const counts = new Array((maxValue as number) + 1);
   array.forEach((element: number) => {
     if (!counts[element]) {
       counts[element] = 0;
@@ -35,21 +37,9 @@ function countingSort(array: number[]) {
   return array;
 }
 
-function findMaxValue(array: number[]) {
-  let max: number = array[0];
-  for (let i = 0; i < array.length; i++) {
-    if (array[i] > max) {
-      max = array[i];
-    }
-  }
-  return max;
-}
-
 let array = [3, 5, 6, 2, 12, 45];
 
-let res = countingSort(array);
-
-console.log(res);
+countingSort(array); // [2, 3, 5, 6, 12, 45]
 
 // 作为模块声明，解决重复块作用域声明错误, ts.2541
 export {};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,13 +2,23 @@
  * @Author: wangshan
  * @Date: 2021-06-24 21:58:10
  * @LastEditors: wangshan
- * @LastEditTime: 2021-07-08 00:19:05
+ * @LastEditTime: 2021-07-08 22:51:05
  * @Description:
  */
 import compareFn from "./compareFn";
 import merge from "./merge";
 import swap from "./Swap";
+import { findMaxValue, findMinValue } from "./min-max-search";
 
 import { IEqualsFunction } from "./ICCompareFunction";
 import { Compare, defaultCompare } from "./compareFn";
-export { compareFn, merge, swap, IEqualsFunction, Compare, defaultCompare };
+export {
+  compareFn,
+  merge,
+  swap,
+  IEqualsFunction,
+  Compare,
+  defaultCompare,
+  findMaxValue,
+  findMinValue,
+};
