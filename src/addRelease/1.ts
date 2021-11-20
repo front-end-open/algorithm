c371af82ec29b6c7f799aeae5157f09dc92af7b6
wangshan <22212@qq.com>
2021-11-20 23:37:05 +0800

    feat: add reslease

---
+++ src/addRelease/1.ts
@@ -0,0 +1,42 @@
+/*
+ * @Author: wangshan
+ * @Date: 2021-11-20 23:34:39
+ * @LastEditors: wangshan
+ * @LastEditTime: 2021-11-20 23:35:55
+ * @Description:
+ */
+class Animal {
+  name: string;
+  constructor(theName: string) {
+    this.name = theName;
+  }
+  move(distanceInMeters: number = 0) {
+    console.log(`${this.name} moved ${distanceInMeters}m.`);
+  }
+}
+
+class Snake extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+  move(distanceInMeters = 5) {
+    console.log("Slithering...");
+    super.move(distanceInMeters);
+  }
+}
+
+class Horse extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+  move(distanceInMeters = 45) {
+    console.log("Galloping...");
+    super.move(distanceInMeters);
+  }
+}
+
+let sam = new Snake("Sammy the Python");
+let tom: Animal = new Horse("Tommy the Palomino");
+
+sam.move();
+tom.move(34);
